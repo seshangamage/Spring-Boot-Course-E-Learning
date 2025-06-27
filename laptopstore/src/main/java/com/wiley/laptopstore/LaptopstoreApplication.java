6a6501fa7326366f3ea78b70fff18abe2b2c4539
Gamage <sgamage@wiley.com>
2025-06-27 20:06:17 +0530

    Initial Project Setup week1

---
+++ laptopstore/src/main/java/com/wiley/laptopstore/LaptopstoreApplication.java
@@ -0,0 +1,13 @@
+package com.wiley.laptopstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopstoreApplication.class, args);
+	}
+
+}
