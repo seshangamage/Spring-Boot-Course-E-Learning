6acbd51f7133da85babd7c06ee71cbe99e01dbdf
Gamage <sgamage@wiley.com>
2025-08-08 20:41:01 +0530

    adding Mobile phone API with discovery service

--- Micoro-services/mobile-phone-backend-api/src/main/java/com/mobile/MobilePhoneApplication.java
+++ Micoro-services/mobile-phone-backend-api/src/main/java/com/mobile/MobilePhoneApplication.java
@@ -2,8 +2,10 @@ package com.mobile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MobilePhoneApplication {
     public static void main(String[] args) {
         SpringApplication.run(MobilePhoneApplication.class, args);
